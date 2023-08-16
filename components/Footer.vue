8088412238ad9a6896235b765465665d7a369805
cededuarte <eduarte.cedric@gmail.com>
2023-08-16 11:10:51 +0800

    aaaa

---
+++ components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p class="text-sm dark:text-slate-100 text-zinc-800">
+      Made with 💩 by
+      <a href="https://github.com/cededuarte" class="underline"
+        >Ced Eduarte</a
+      >
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
